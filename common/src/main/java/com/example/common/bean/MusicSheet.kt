516d9c9326e44fb4992fbcf95b32da30d26cec2a
Kokomi <939826156@qq.com>
2022-10-10 21:31:16 +0800

    修改:现在主模块引用了common模块，Application使用common模块下的MyApplication，为每个模块都加上了dataBinding支持，主题为NoActionBar，部分命名错误和库不兼容问题

--- common/src/main/java/com/example/common/bean/MusicSheet.kt
+++ common/src/main/java/com/example/common/bean/MusicSheet.kt
@@ -1,3 +1,20 @@
 package com.example.common.bean
 
-data class MusicSheet()
+data class MusicSheet(
+    /**
+     * 歌单 ID
+     * */
+    val id: Long,
+    /**
+     * 歌单名
+     * */
+    val name: String,
+    /**
+     * 歌单描述
+     * */
+    val des: String,
+    /**
+     * 歌单图片
+     * */
+    val img: String
+)
--- preference/build.gradle.kts
+++ preference/build.gradle.kts
@@ -11,7 +11,6 @@ android {
         targetSdk = 32
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -43,9 +42,10 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.5.0")
-    implementation("com.google.android.material:material:1.6.1")
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
