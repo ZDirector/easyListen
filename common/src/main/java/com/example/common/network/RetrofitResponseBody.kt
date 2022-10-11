c973febd5afd530043f2034c2e9974f46776b96b
Kokomi <939826156@qq.com>
2022-10-12 00:23:25 +0800

    令GSON适配了网易云接口的返回结果

--- common/src/main/java/com/example/common/network/RetrofitResponseBody.kt
+++ common/src/main/java/com/example/common/network/RetrofitResponseBody.kt
@@ -1,7 +1,11 @@
 package com.example.common.network
 
+import com.google.gson.annotations.SerializedName
+
 data class RetrofitResponseBody<T>(
+    @SerializedName("code")
     val status: Int,
     val message: String,
+    @SerializedName("result")
     val data: T?
 )
--- search/src/main/java/com/example/search/service/SearchService.kt
+++ search/src/main/java/com/example/search/service/SearchService.kt
@@ -1,6 +1,7 @@
 package com.example.search.service
 
 import com.example.common.bean.SearchSuggestBean
+import com.example.common.bean.SearchSuggestResult
 import com.example.common.network.RetrofitResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,5 +10,5 @@ interface SearchService {
     @GET("/search/suggest")
     suspend fun getSearchSuggest(
         @Query("keywords") keyWords : String,
-        @Query("type") type : String = "mobile") : RetrofitResponseBody<SearchSuggestBean>
+        @Query("type") type : String = "mobile") : RetrofitResponseBody<SearchSuggestResult>
 }
