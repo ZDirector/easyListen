4a005af978ced1ae0f616c9ba859f7043db32d9e
Kokomi <939826156@qq.com>
2022-10-11 22:01:51 +0800

    修改showToast函数BUG

--- app/src/main/java/com/example/easylisten/MainActivity.kt
+++ app/src/main/java/com/example/easylisten/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.example.easylisten
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.kokomi.preference.TestActivity
+import com.example.common.utils.showToast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivity(Intent(this, TestActivity::class.java))
+        showToast("abc")
     }
 }
