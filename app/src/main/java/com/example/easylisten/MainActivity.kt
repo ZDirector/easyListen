58a11eed5b232f3f83253c345717fd6b44d3830c
“tangan” <215351611@qq.com>
2022-10-11 22:26:28 +0800

    git 测试一下

--- app/src/main/java/com/example/easylisten/MainActivity.kt
+++ app/src/main/java/com/example/easylisten/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.easylisten
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.common.baseui.BaseActivity
+import com.example.easylisten.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity(override val layoutId: Int = R.layout.activity_main) : BaseActivity<ActivityMainBinding,MainViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun initData(savedInstanceState: Bundle?) {
+        println("我，俄欧皮$viewModel  ${viewModel.ss}")
     }
 }
---
+++ app/src/main/java/com/example/easylisten/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.easylisten
+
+import com.example.common.baseui.BaseViewModel
+
+class MainViewModel: BaseViewModel() {
+    val ss = 111
+}
