a673b8953cf00bfb1f73ffd4d7e7e20393d5f146
“tangan” <215351611@qq.com>
2022-10-11 23:22:17 +0800

    feat(master)-修改一点

--- homepage/src/main/java/com/example/homepage/HomeActivity.kt
+++ homepage/src/main/java/com/example/homepage/HomeActivity.kt
@@ -1,6 +1,6 @@
 package com.example.homepage
 
-import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity 
 import android.os.Bundle
 
 class HomeActivity : AppCompatActivity() {
