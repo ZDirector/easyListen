6f98ab28191b33eb170bf6629cc535dd237871cb
Z_Director <270523826@qq.com>
2022-10-12 15:00:39 +0800

    暂时提交

--- search/src/main/java/com/example/search/service/SearchService.kt
+++ search/src/main/java/com/example/search/service/SearchService.kt
@@ -1,7 +1,6 @@
 package com.example.search.service
 
 import com.example.common.bean.SearchSuggestBean
-import com.example.common.network.RetrofitResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -9,5 +8,5 @@ interface SearchService {
     @GET("/search/suggest")
     suspend fun getSearchSuggest(
         @Query("keywords") keyWords : String,
-        @Query("type") type : String = "mobile") : RetrofitResponseBody<SearchSuggestBean>
+        @Query("type") type : String = "mobile") : SearchSuggestBean
 }
