a1c2e10db62fe8c87e47bc877e36b5d41a28bfbf
“tangan” <215351611@qq.com>
2022-10-19 00:21:36 +0800

    feat(gangan) 主界面控制播放的布局

--- homepage/src/main/java/com/example/homepage/HomeActivity.kt
+++ homepage/src/main/java/com/example/homepage/HomeActivity.kt
@@ -5,7 +5,6 @@ import android.os.Build
 import android.os.Bundle
 import android.view.ViewGroup
 import android.view.WindowManager
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentManager
