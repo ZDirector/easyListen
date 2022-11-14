0a0e9661b41a8d0257134088f5182ca936f2ad31
“tangan” <215351611@qq.com>
2022-11-15 01:13:42 +0800

    feat(gangan) -删除没必要import

--- homepage/src/main/java/com/example/homepage/toplist/viewmodel/TopListViewModel.kt
+++ homepage/src/main/java/com/example/homepage/toplist/viewmodel/TopListViewModel.kt
@@ -2,15 +2,12 @@ package com.example.homepage.toplist.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.homepage.toplist.bean.Song
 import com.example.homepage.toplist.bean.TopDetail
 import com.example.homepage.toplist.bean.TopListTab
 import com.example.homepage.toplist.repostory.TopListRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import kotlin.concurrent.fixedRateTimer
 
 class TopListViewModel : ViewModel() {
     private val _topDetailsStateFlow = MutableStateFlow(emptyMap<TopListTab,List<TopDetail>>())
